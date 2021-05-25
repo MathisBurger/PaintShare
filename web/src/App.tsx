d3951a5aedbb36b0ffbe19758f3c26e9c297ed79
Mathis <handymathis@gmail.com>
2021-05-25 22:02:58 +0200

    added base for explore page [#8]

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,6 +7,7 @@ import Login from "./routes/Login";
 import Register from "./routes/Register";
 import Dashboard from "./routes/dashboard";
 import Profile from "./routes/profile";
+import ExploreRoute from "./routes/Explore";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path={"/dashboard"} component={Dashboard} />
         <Route path={"/profile"} component={Profile} />
         <Route path={"/user/:name"} component={Profile} />
+        <Route path={"/explore"} component={ExploreRoute} />
         <Redirect to={"/dashboard"} />
       </Switch>
     </Router>
---
+++ web/src/routes/Explore.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import DesignWrapper from "../components/designWrapper";
+
+export default class ExploreRoute extends React.Component {
+
+    render() {
+        return (
+            <>
+                <DesignWrapper>
+                    <div>
+                        Here should be an input field to search for usernames
+                        Straight down here should be some great svg to fill the contentless page with a background
+                        It should be something like a colorful paint
+                    </div>
+                </DesignWrapper>
+            </>
+        );
+    }
+}
